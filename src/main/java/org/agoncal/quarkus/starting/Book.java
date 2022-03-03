513e42b45131a45b62722204edd45d157badb681
Kumbirai Mundangepfupfu <kumbirai.mundangepfupfu@partner.bmw.co.za>
2022-03-03 11:15:30 +0200

    Commit after creation

---
+++ src/main/java/org/agoncal/quarkus/starting/Book.java
@@ -0,0 +1,19 @@
+package org.agoncal.quarkus.starting;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class Book
+{
+    private int id;
+    private String title;
+    private String author;
+    private int yearOfPublication;
+    private String genre;
+}
